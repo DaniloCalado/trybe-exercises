cfefb5e459b557b489b09963084b3a8d8fd7aaba
Danilo <dcalado.fisio@yahoo.com.br>
2021-07-28 18:40:44 -0300

    exercicio 7 nao resolvido

---
+++ fundamentos/bloco-4-introducao-a-jacascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio 7.js
@@ -0,0 +1,17 @@
+let nota = -1;
+
+if (nota >= 90){
+    console.log("A")
+} else if (nota >= 80){
+    console.log("B")
+} else if (nota >= 70){
+    console.log("C")
+} else if (nota >= 60){
+    console.log("D")
+} else if (nota >= 50){
+    console.log("E")
+} else if (nota < 50){
+    console.log("F")
+} else if (nota > 100 || nota < 0){
+    console.log("ERRO! este aplicativo será fechado!")
+} 
