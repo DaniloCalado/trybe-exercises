1cf677d7eadde2ca595a47a55c4824224303cbc1
Danilo <dcalado.fisio@yahoo.com.br>
2021-07-28 16:05:19 -0300

    adicionado pasta bloco 4 JS

---
+++ fundamentos/bloco-4-introducao-a-jacascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -0,0 +1,18 @@
+let nota = 60;
+
+switch(nota){
+    case ">=80":
+        console.log("VocÊ foi aprovado");
+        break;
+
+    case ">=60":
+        console.log("VocÊ está na lista de espera");
+        break;
+
+    case "<60":
+        console.log("VocÊ foi reprovado");
+       break;
+}
+
+
+
