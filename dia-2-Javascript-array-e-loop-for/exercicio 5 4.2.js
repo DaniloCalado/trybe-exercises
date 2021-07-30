188ca903f4180450079445fc5fac62aeff9a6ebc
Danilo <dcalado.fisio@yahoo.com.br>
2021-07-30 02:01:31 -0300

    exercicio 5

---
+++ dia-2-Javascript-array-e-loop-for/exercicio 5 4.2.js
@@ -0,0 +1,17 @@
+/* Utilizando for , descubra qual o maior valor contido no array e imprima-o;*/
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let maiorNumero = 0;
+
+for(let i = 0;i < numbers.length;i++){
+    if(numbers[i] > maiorNumero){
+        maiorNumero = numbers[i]
+    }
+}
+
+console.log(maiorNumero)
+
+
+
+
